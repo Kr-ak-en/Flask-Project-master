7fbc34821520f5c4e90cc6a4e92f72f56d1732e1
Kr-ak-en <76852104+Kr-ak-en@users.noreply.github.com>
2021-04-26 17:28:30 +0500

    Update main.py

--- Flask-Project-master/main.py
+++ Flask-Project-master/main.py
@@ -46,7 +46,7 @@ def index():
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def reqister():
+def register()
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
